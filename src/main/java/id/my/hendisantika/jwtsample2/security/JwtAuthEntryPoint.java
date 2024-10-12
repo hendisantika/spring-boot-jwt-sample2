c28fdcbc4527da181c64b077654742ca9beeb590
hendisantika <hendisantika@yahoo.co.id>
2024-10-12 13:33:18 +0700

    Add JwtAuthEntryPoint🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/jwtsample2/security/JwtAuthEntryPoint.java
@@ -0,0 +1,29 @@
+package id.my.hendisantika.jwtsample2.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-sample2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/10/24
+ * Time: 13.30
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+}
