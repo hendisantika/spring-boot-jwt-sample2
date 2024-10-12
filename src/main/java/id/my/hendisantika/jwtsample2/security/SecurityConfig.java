6c0b7951050d63ac911b2a529e615675bddf787a
hendisantika <hendisantika@yahoo.co.id>
2024-10-12 13:39:26 +0700

    Add filterChain in SecurityConfig🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/jwtsample2/security/SecurityConfig.java
+++ src/main/java/id/my/hendisantika/jwtsample2/security/SecurityConfig.java
@@ -2,8 +2,13 @@ package id.my.hendisantika.jwtsample2.security;
 
 import id.my.hendisantika.jwtsample2.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +29,25 @@ public class SecurityConfig {
 
     private final CustomUserDetailsService userDetailsService;
     private final JwtAuthEntryPoint authEntryPoint;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authz) -> authz.requestMatchers("/api/**")
+                .permitAll()
+                .requestMatchers(HttpMethod.GET)
+                .permitAll()
+                .requestMatchers(HttpMethod.DELETE)
+                .hasAuthority(ADMIN)
+                .requestMatchers(HttpMethod.POST)
+                .permitAll()
+                .requestMatchers(HttpMethod.PUT)
+                .hasAuthority(USER)
+                .anyRequest()
+                .authenticated());
+
+        http.exceptionHandling((exceptions) -> exceptions.authenticationEntryPoint(authEntryPoint));
+        http.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.csrf(c -> c.disable());
+        return http.build();
+    }
 }
