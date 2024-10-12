c0e4c89d26879d5e811df085b3683ec7f2e1c01f
hendisantika <hendisantika@yahoo.co.id>
2024-10-12 13:36:40 +0700

    Add doFilterInternal in JwtAuthenticationFilter🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/jwtsample2/security/JwtAuthenticationFilter.java
+++ src/main/java/id/my/hendisantika/jwtsample2/security/JwtAuthenticationFilter.java
@@ -1,8 +1,23 @@
 package id.my.hendisantika.jwtsample2.security;
 
 import id.my.hendisantika.jwtsample2.service.CustomUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,5 +36,28 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private JwtGenerator tokenGenerator;
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String token = getJWTFromRequest(request);
+        if (StringUtils.hasText(token) && tokenGenerator.validateToken(token)) {
+            String username = tokenGenerator.getUsernameFromJWT(token);
+
+            List<String> roles = tokenGenerator.getRolesFromJWT(token);
+            List<GrantedAuthority> authorities = roles.stream()
+                    .map(role -> new SimpleGrantedAuthority(role.replace("ROLE_", "")))
+                    .collect(Collectors.toList());
+
+            UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(userDetails, null, authorities);
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+        }
+        filterChain.doFilter(request, response);
+    }
+
 
 }
