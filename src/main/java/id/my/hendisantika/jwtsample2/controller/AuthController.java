fbd8ee2dae13da4e18149bcabafa497ec2734bd6
hendisantika <hendisantika@yahoo.co.id>
2024-10-12 13:49:23 +0700

    Add register in AuthController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/jwtsample2/controller/AuthController.java
+++ src/main/java/id/my/hendisantika/jwtsample2/controller/AuthController.java
@@ -1,13 +1,22 @@
 package id.my.hendisantika.jwtsample2.controller;
 
+import id.my.hendisantika.jwtsample2.dto.RegisterDto;
+import id.my.hendisantika.jwtsample2.model.AppUser;
+import id.my.hendisantika.jwtsample2.model.Role;
 import id.my.hendisantika.jwtsample2.repository.AppUserRepository;
 import id.my.hendisantika.jwtsample2.repository.RoleRepository;
 import id.my.hendisantika.jwtsample2.security.JwtGenerator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,4 +38,22 @@ public class AuthController {
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
     private final JwtGenerator jwtGenerator;
+
+    @PostMapping("register")
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
+        if (appUserRepository.existsByUsername(registerDto.getUsername())) {
+            return new ResponseEntity<>("Username is taken!", HttpStatus.BAD_REQUEST);
+        }
+
+        AppUser appUser = new AppUser();
+        appUser.setUsername(registerDto.getUsername());
+        appUser.setPassword(passwordEncoder.encode((registerDto.getPassword())));
+
+        Role roles = roleRepository.findByName(registerDto.getRole()).get();
+        appUser.setRoles(Collections.singletonList(roles));
+
+        appUserRepository.save(appUser);
+
+        return new ResponseEntity<>("User registered success!", HttpStatus.OK);
+    }
 }
