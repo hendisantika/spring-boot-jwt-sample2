80e34b5f6959e6060f416a6e9017c1c6ac970855
hendisantika <hendisantika@yahoo.co.id>
2024-10-12 13:27:29 +0700

    Add loadUserByUsername in CustomUserDetailsService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/jwtsample2/service/CustomUserDetailsService.java
+++ src/main/java/id/my/hendisantika/jwtsample2/service/CustomUserDetailsService.java
@@ -1,9 +1,20 @@
 package id.my.hendisantika.jwtsample2.service;
 
+import id.my.hendisantika.jwtsample2.model.AppUser;
+import id.my.hendisantika.jwtsample2.model.Role;
 import id.my.hendisantika.jwtsample2.repository.AppUserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +32,14 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private final AppUserRepository appUserRepository;
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser appUser = appUserRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return new User(appUser.getUsername(), appUser.getPassword(), mapRolesToAuthorities(appUser.getRoles()));
+    }
+
+    private Collection<GrantedAuthority> mapRolesToAuthorities(List<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
 }
