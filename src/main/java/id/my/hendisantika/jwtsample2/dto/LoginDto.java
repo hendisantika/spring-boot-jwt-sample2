39b36b042e0257387083bf458bcf63bbaa2b8aea
hendisantika <hendisantika@yahoo.co.id>
2024-10-12 10:49:36 +0700

    Add LoginDto🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/jwtsample2/dto/LoginDto.java
@@ -0,0 +1,19 @@
+package id.my.hendisantika.jwtsample2.dto;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-sample2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/10/24
+ * Time: 10.49
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+public class LoginDto {
+    private String username;
+    private String password;
+}
