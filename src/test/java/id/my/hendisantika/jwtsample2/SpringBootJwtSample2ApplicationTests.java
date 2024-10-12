1fe2dfb97788378cd4a0e1545315521dd975e920
hendisantika <hendisantika@yahoo.co.id>
2024-10-13 06:28:20 +0700

    Add TestContainer step🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/jwtsample2/SpringBootJwtSample2ApplicationTests.java
+++ src/test/java/id/my/hendisantika/jwtsample2/SpringBootJwtSample2ApplicationTests.java
@@ -1,13 +1,34 @@
 package id.my.hendisantika.jwtsample2;
 
-import org.junit.jupiter.api.Test;
+import id.my.hendisantika.jwtsample2.repository.AppUserRepository;
+import id.my.hendisantika.jwtsample2.repository.RoleRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@Testcontainers
+@SpringBootTest(
+        properties = {
+                "management.endpoint.health.show-details=always",
+                "spring.datasource.url=jdbc:tc:postgresql:17.0-alpine3.20:///bankDB"
+        },
+        webEnvironment = RANDOM_PORT
+)
 class SpringBootJwtSample2ApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private AppUserRepository appUserRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @BeforeEach
+    void deleteAll() {
+        appUserRepository.deleteAll();
+        roleRepository.deleteAll();
     }
 
 }
