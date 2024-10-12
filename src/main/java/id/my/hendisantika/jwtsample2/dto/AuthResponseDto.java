fb71b82e36716fa7fe46cf5cb85c6fc59ce914c7
hendisantika <hendisantika@yahoo.co.id>
2024-10-12 10:49:18 +0700

    Add AuthResponseDto🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/jwtsample2/dto/AuthResponseDto.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.jwtsample2.dto;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-sample2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/10/24
+ * Time: 10.49
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+public class AuthResponseDto {
+    private String accessToken;
+    private String tokenType = "Bearer ";
+
+    public AuthResponseDto(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
