374f0d1b71e0555353d549b9ec945c415e78d131
hendisantika <hendisantika@yahoo.co.id>
2024-10-12 13:35:11 +0700

    Add generateToken in JwtGenerator🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/jwtsample2/security/JwtGenerator.java
+++ src/main/java/id/my/hendisantika/jwtsample2/security/JwtGenerator.java
@@ -1,10 +1,17 @@
 package id.my.hendisantika.jwtsample2.security;
 
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +28,24 @@ public class JwtGenerator {
 
     public long JWT_EXPIRATION = 70000;
     private final Key secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + JWT_EXPIRATION);
+
+        // Extract roles from the authentication object
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        List<String> roles = authorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList());
+
+        // Build the token with roles included in claims
+        String token = Jwts
+                .builder()
+                .setSubject(username)
+                .claim("roles", roles) // Inclusion of roles in claims
+                .setIssuedAt(new Date())
+                .setExpiration(expireDate)
+                .signWith(SignatureAlgorithm.HS512, secretKey).compact();
+        return token;
+    }
 }
