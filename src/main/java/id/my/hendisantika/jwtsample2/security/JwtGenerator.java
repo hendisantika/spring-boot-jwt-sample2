08a63d3eeee01d444c102c86f089ceb012e46804
hendisantika <hendisantika@yahoo.co.id>
2024-10-12 13:35:31 +0700

    Add validateToken in JwtGenerator🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/jwtsample2/security/JwtGenerator.java
+++ src/main/java/id/my/hendisantika/jwtsample2/security/JwtGenerator.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
@@ -53,5 +54,15 @@ public class JwtGenerator {
     public String getUsernameFromJWT(String token) {
         Claims claims = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody();
         return claims.getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception ex) {
+            throw new AuthenticationCredentialsNotFoundException("JWT was exprired or incorrect",
+                    ex.fillInStackTrace());
+        }
     }
 }
